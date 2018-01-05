1b256d1764474d5af380da6fafbb10d23f6d4c60
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-04 22:29:17 -0800

    Almost done

--- W12D4/quicksort/lib/quick_sort.rb
+++ W12D4/quicksort/lib/quick_sort.rb
@@ -13,10 +13,30 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-
+    return if array.length < 2
+    pivot = partition(array, start, length, &prc)
+    left = pivot - start
+    right = length - 1 - left
+    sort2!(array, start, left, &prc)
+    sort2!(array, pivot+1, right, &prc)
+    array
   end
 
   def self.partition(array, start, length, &prc)
-    
+    # p array
+    # p start
+    # p length
+    # p prc
+    temp = start
+    pivot = array[start]
+
+    (start+1..(start+length)-1).each do |ind|
+      if array[ind] <= pivot
+        array[ind], array[temp+1] = array[temp+1], array[ind]
+        temp += 1
+      end
+    end
+    array[start], array[temp] = array[temp], array[start]
+    temp
   end
 end
