216e3d2fe440a3f5e0a649cccb0bcb60e54bb00a
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-04 20:08:34 -0800

    Finished part 1

--- W12D4/quicksort/lib/quick_sort.rb
+++ W12D4/quicksort/lib/quick_sort.rb
@@ -4,12 +4,19 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return if array.length < 2
+    pivot = array.first
+    left = array[1..-1].select { |e| e <= pivot }
+    right = array[1..-1].select {|e| e > pivot}
+    QuickSort.sort1(left) + [pivot] + QuickSort.sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+
   end
 
   def self.partition(array, start, length, &prc)
+    
   end
 end
