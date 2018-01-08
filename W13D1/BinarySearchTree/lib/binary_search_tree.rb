183cd19638408c6b5255c1ddc2efa40c17ca1803
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-08 01:10:49 -0800

    A little better

--- W13D1/BinarySearchTree/lib/binary_search_tree.rb
+++ W13D1/BinarySearchTree/lib/binary_search_tree.rb
@@ -12,26 +12,77 @@ class BinarySearchTree
   def insert(value)
     # p @root
     # p value
-    if @root.nil?
-      @root = BSTNode.new(value)
-    elsif @root.left.nil? && @root.right.nil? && @root > value
-      @root.left = BSTNode.new(value)
-    elsif @root.left.nil? && @root.right.nil? && @root < value
-      @root.right = BSTNode.new(value)
+    return @root = BSTNode.new(value) if @root.nil?
+    step = @root
+    while true
+      if value > step.value
+        if step.right.nil?
+          step.right = BSTNode.new(value)
+          step.right.parent = step
+          break
+        else
+          step = step.right
+        end
+      else
+        if step.left.nil?
+          step.left = BSTNode.new(value)
+          step.left.parent = step
+          break
+        else
+          step = step.left
+        end
+      end
     end
   end
 
   def find(value, tree_node = @root)
+    return tree_node if value == tree_node.value
+    return nil if tree_node.nil?
+
+    step = tree_node
+    while true
+      # p value
+      # p step.value
+      if value > step.value
+        if step.right.nil?
+          return nil
+        elsif step.right.value == value
+          return step.right
+        else
+          step = step.right
+        end
+      else
+        if step.left.nil?
+          return nil
+        elsif step.left.value == value
+          return step.left
+        else
+          step = step.left
+        end
+      end
+    end
   end
 
   def delete(value)
+    # p value
+    # p @root
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    step = tree_node
+    until step.right.nil?
+      if step.value < step.right.value
+        step = step.right
+      end
+    end
+    step
   end
 
   def depth(tree_node = @root)
+    return 0 if tree_node.nil?
+    count = 0
+    
   end
 
   def is_balanced?(tree_node = @root)
--- W13D1/BinarySearchTree/lib/bst_node.rb
+++ W13D1/BinarySearchTree/lib/bst_node.rb
@@ -1,11 +1,12 @@
 class BSTNode
 
   attr_reader :value
-  attr_accessor :left, :right
+  attr_accessor :left, :right, :parent
 
   def initialize(value)
     @value = value
     @left
     @right
+    @parent
   end
 end
