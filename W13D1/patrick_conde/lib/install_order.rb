fd630f5db93039dd3b7550868757254c7721e1d5
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-09 11:23:00 -0800

    Almost finished

--- W13D1/patrick_conde/lib/install_order.rb
+++ W13D1/patrick_conde/lib/install_order.rb
@@ -6,16 +6,32 @@
 # N.B. this is how `npm` works.
 
 # Import any files you need to
+require_relative 'graph'
+require_relative 'topological_sort'
 
 
 
 def install_order(arr)
   p arr
 
-  flatten_arr = arr.flatten
-  max_id = flatten_arr.sort[-1]
+  package_ids = []
+  arr.each do |sub_arr|
+    package_ids << sub_arr[0]
+  end
+  max_id = package_ids.sort[-1]
+
   new_arr = []
+
   (1..max_id).each do |ind|
-    new_arr = []
+    new_arr << Vertex.new(ind)
+  end
+
+  # edges = []
+  # arr.each do |sub_arr|
+  #   edges << Edge.new(sub_arr[1], sub_arr[0])
+  # end
+
+  topological_sort(new_arr).map do |vertex|
+    vertex.value
   end
 end
